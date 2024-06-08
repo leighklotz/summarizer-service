08f9a7186760166785f528a7b22eb474a048f637
Leigh Klotz <klotz@klotz.me>
2024-06-09 00:15:16 +0100

    run.sh: naive runner, logs to tmp

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the script directory from $0
+SCRIPT_DIR="$( dirname "$0" )"
+
+cd "${SCRIPT_DIR}"
+
+# Use a consistent date syntax for YYYYMMDD-HHMMSS
+stamp="$(date +"%Y%m%d-%H%M%S")"
+
+# Ensure the log directory exists
+if [ ! -d "/tmp/summarizer" ]; then
+   mkdir -p "/tmp/summarizer"
+fi
+
+# Use double quotes around file name to handle spaces in log file names
+./app.py 2&>1 >> "/tmp/summarizer/${stamp}-$$.log"
