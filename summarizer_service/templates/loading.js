49e86998cae1c35ccca161a0dc80ebca2eca269d
Leigh Klotz <klotz@klotz.me>
2024-08-12 16:44:03 -0700

    markdown: tab ui support In summarizer, change from Markdown checkbox to Text and Markdown tabs. Place the tab buttons in line with the 'Summarize' submit button to preserve vertical spce. May need a better design. - Add form ID to cards scuttle, summarize, and via-api-model to fix JavaScript event listener issues. - Update JavaScript loading script to use the new form ID. - Remove redundant console logs and comments from markdown.js and tabs.css. - Update tab-button class and active state styles for consistency. - For summarizer, made an attempt to make the submit button the same height as the other tab buttons, but still have failed.

--- summarizer_service/templates/loading.js
+++ summarizer_service/templates/loading.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var form = document.querySelector("form");
-    if (form) {
+    var f = document.getElementById("mainform");
+    console.log("form", f);
+    if (f) {
 	var submitButton = document.getElementById("submit-button");
 	var loadingAnimation = document.getElementById("loading");
 
-	form.addEventListener("submit", function() {
+	f.addEventListener("submit", function() {
             submitButton.disabled = true;
             loadingAnimation.style.display = "block";
 	});
--- summarizer_service/templates/markdown.js
+++ summarizer_service/templates/markdown.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const output = document.getElementById('output');
 
     function selectTextTab() {
-        console.log("textTab click", textTab);
+        // console.log("textTab click", textTab);
         textTab.classList.add('active');
         markdownTab.classList.remove('active');
 	output.classList.remove('display');
@@ -15,14 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function selectMarkdownTab() {
-	console.log("markdownTab click", markdownTab);
-	console.log("markdownTab innerText", input.innerText);
+	// console.log("markdownTab click", markdownTab);
+	// console.log("markdownTab innerText", input.innerText);
         markdownTab.classList.add('active');
         textTab.classList.remove('active');
 
         // todo: dom sanitize
         let v = marked.parse(input.innerText);
-	console.log("markdownTab: setting output.innerHTML to  v", v);
+	// console.log("markdownTab: setting output.innerHTML to  v", v);
         output.innerHTML = v;
 
         output.classList.add('display');
@@ -33,17 +33,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     if (textTab)  {
-        console.log("adding event listener to textTab", textTab);
+        // console.log("adding event listener to textTab", textTab);
         textTab.addEventListener('click', selectTextTab);
     }
 
     if (markdownTab) {
-        console.log("adding event listener to markdownTab", markdownTab);
+        // console.log("adding event listener to markdownTab", markdownTab);
         markdownTab.addEventListener('click', selectMarkdownTab);
     }
 
     if (markdownTab) {
-	console.log("calling setupMarkdownDisplay");
+	// console.log("calling setupMarkdownDisplay");
 	selectMarkdownTab();
     }
 });
