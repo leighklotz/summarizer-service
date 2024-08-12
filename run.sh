bcb34cf230c4028fd1c9895a8c74dffe4d7560c4
Leigh Klotz <klotz@klotz.me>
2024-08-12 12:06:21 -0700

    Update run.sh for better logging; Add logging to application.py; Update templates for UI enhancements

--- run.sh
+++ run.sh
@@ -2,19 +2,4 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
 
-### 
-### SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
-### 
-### # Use a consistent date syntax for YYYYMMDD-HHMMSS
-### stamp="$(date +"%Y%m%d-%H%M%S")"
-### 
-### # Ensure the log directory exists
-### if [ ! -d "/tmp/summarizer" ]; then
-###    mkdir -p "/tmp/summarizer"
-### fi
-### 
-### # Use double quotes around file name to handle spaces in log file names
-### ${SCRIPT_DIR}/app.py 2>&1 | tee >> "/tmp/summarizer/${stamp}-$$.log"
-###
-
-gunicorn -b 0.0.0.0:8080 --timeout 300 summarizer_service:app
+gunicorn --workers=2 --log-level=info --access-logfile - -b 0.0.0.0:8080 --timeout 300 summarizer_service:app
