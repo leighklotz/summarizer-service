c282041af8cd7c24ee2a4f27475c36f43d30f4d2
Leigh Klotz <klotz@klotz.me>
2024-08-13 15:44:26 -0700

    markdown: Remove unnecessary console log in loading.js

--- summarizer_service/templates/loading.js
+++ summarizer_service/templates/loading.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     var f = document.getElementById("mainform");
-    console.log("form", f);
     if (f) {
 	var submitButton = document.getElementById("submit-button");
 	var loadingAnimation = document.getElementById("loading");
