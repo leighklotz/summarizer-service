4933ceb6603ee16b4806368d223fa4c0387310ec
Leigh Klotz <klotz@klotz.me>
2025-03-04 17:03:36 -0800

    Update run.sh to append script directory to PATH

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
 cd "${SCRIPT_DIR}"
 
-# export SECRET_KEY="$(python3 -c 'import os; print(os.urandom(24).hex())')"
+export PATH="${PATH}":"${SCRIPT_DIR}"
 export SECRET_KEY="$(openssl rand -hex 24)"
 gunicorn --workers=2 --log-level=info --access-logfile - -b 0.0.0.0:8080 --timeout 300 summarizer_service:app --limit-request-line 65535
