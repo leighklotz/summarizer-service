95d3201849358970f3d61ede7f8a2c8d086652bb
Leigh Klotz <klotz@klotz.me>
2025-03-04 18:04:10 -0800

    Refactor run.sh to source summarizer_service/config.py as env vars; modify SummarizeCard prompts; clean up config.py.example

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
 cd "${SCRIPT_DIR}"
 
 export PATH="${PATH}":"${SCRIPT_DIR}"
+source "${SCRIPT_DIR}/summarizer_service/config.py" 
 export SECRET_KEY="$(openssl rand -hex 24)"
-gunicorn --workers=2 --log-level=info --access-logfile - -b 0.0.0.0:8080 --timeout 300 summarizer_service:app --limit-request-line 65535
+
+via --get-via || exit 1
+
+gunicorn --workers=2 --log-level=info --access-logfile - -b ${LISTEN_HOST}:${PORT} --timeout 300 summarizer_service:app --limit-request-line 65535
